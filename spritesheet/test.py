aca42dba1de04d0dbcfb8ff808e25cec4bd06a4f
ArthurVerwerss <aah.arthur@gmail.com>
2024-10-03 15:22:00 +0200

    wip spritesheet

--- spritesheet/test.py
+++ spritesheet/test.py
@@ -10,8 +10,10 @@ game_window = pygame.display.set_mode((win_width, win_height))
 canvas = pygame.Surface((win_width, win_height))
 pygame.display.set_caption("Moving Object Using PyGame")
 
-my_spritesheet = Spritesheet('bike_sheet.png')
-bike1 = my_spritesheet.get_sprite(0,0,288,222)
+my_spritesheet = Spritesheet('spritesheet/bike_sheet.png')
+bikes = [my_spritesheet.parse_sprite('bike_color.png'), my_spritesheet.parse_sprite('bike2.png')]
+index = 0
+
 running = True
 while running:
 
@@ -19,9 +21,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                index = (index + 1) % len(bikes)
 
     canvas.fill((255,255,255))
-    canvas.blit(bike1, (0, win_height - 288))
+    canvas.blit(bikes[index], (0, win_height - 288))
     game_window.blit(canvas, (0,0))
     pygame.display.update()
 
